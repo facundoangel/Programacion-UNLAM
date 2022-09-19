b611c61c778e8950f50a823e91349050052e5e5f
Facundo <77032020+facundoangel@users.noreply.github.com>
2022-09-19 11:50:38 -0300

    agregado de practica del algoritmo merge

---
+++ archivos-merge-c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "archivos.h"
+
+int main()
+{
+    //genrarArchivos();
+    mostrarArchivos();
+
+
+    mergeAlgoritmo("productos.dat","movimientos.dat");
+
+    mostrarArchivos();
+    void mostrarArchivoTMP();
+
+
+    return 0;
+}
